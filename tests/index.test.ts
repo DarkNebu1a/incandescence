8ae19715a44e601ac0e499f974be5e5223718e41
Craftinators <craftinatorssx@gmail.com>
2023-02-23 02:30:23 -0500

    chore: add `Lazy` tests
    Signed-off-by: Craftinators <craftinatorssx@gmail.com>

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,7 +1,25 @@
 import { describe, it, expect } from "vitest";
+import { Lazy } from "../src";
 
-describe("Whatever", () => {
-  it("should pass CI", () => {
-    expect(1).toBe(1);
+describe("Lazy", () => {
+  it("toString() should throw error", () => {
+    const lazy = new Lazy<number[]>(() => []);
+    expect(() => lazy.toString()).toThrowError("Value has not been instantiated yet.");
+  });
+
+  it("value should have length 3", () => {
+    const lazy = new Lazy(() => [4, 5, 6]);
+    expect(lazy.value).toHaveLength(3);
+  });
+
+  it("value should not be instantiated", () => {
+    const lazy = new Lazy<number[]>(() => []);
+    expect(lazy.instantiated).toBe(false);
+  });
+
+  it("value should be instantiated", () => {
+    const lazy = new Lazy<number[]>(() => []);
+    lazy.value;
+    expect(lazy.instantiated).toBe(true);
   });
 });
